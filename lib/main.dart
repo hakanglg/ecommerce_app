86590d267861569acf12e0283f31da34210e94cb
hakanglg <hakan_golge@hotmail.com>
2022-01-07 02:27:28 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_app/constants.dart';
+import 'package:ecommerce_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
